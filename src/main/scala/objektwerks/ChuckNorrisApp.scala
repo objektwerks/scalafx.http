6d057d913ec5b45d1be11389ad9c8eeb597ef4ba
objektwerks <objektwerks@runbox.com>
2023-01-26 16:09:19 -0500

    refactored app

--- src/main/scala/objektwerks/ChuckNorrisApp.scala
+++ src/main/scala/objektwerks/ChuckNorrisApp.scala
@@ -1,20 +1,8 @@
 package objektwerks
 
-import akka.actor.ActorSystem
-
-import com.typesafe.config.ConfigFactory
-
-import scalafx.Includes.*
 import scalafx.application.JFXApp3
-import scalafx.scene.Scene
-
-import scala.concurrent.ExecutionContext
 
 object ChuckNorrisApp extends JFXApp3:
-  val conf = ConfigFactory.load("app.conf")
-  given system: ActorSystem = ActorSystem.create("chuck-norris", conf)
-  given dispatcher: ExecutionContext = system.dispatcher
-
   override def start(): Unit =
     val view = ChuckNorrisView()
     stage = new JFXApp3.PrimaryStage:
