da722e51d782ae0ef13f0edf4e418efe073345fb
objektwerks <objektwerks@runbox.com>
2023-01-26 18:04:59 -0500

    edited view

--- src/main/scala/objektwerks/ChuckNorrisView.scala
+++ src/main/scala/objektwerks/ChuckNorrisView.scala
@@ -16,45 +16,45 @@ import java.util.concurrent.Executor
 class ChuckNorrisView(executor: Executor, uri: URI):
   def logo = new Image(Image.getClass.getResourceAsStream("/cn.jpg"))
 
+  val webview = new WebView()
+
   val jokeProperty = ObjectProperty[String]("")
   jokeProperty.onChange { (_, _, newJoke) =>
-    jokeWebView.engine.loadContent(newJoke)
+    webview.engine.loadContent(newJoke)
   }
 
-  val jokeWebView = new WebView()
-
-  val jokeWebViewPane = new VBox:
-    id = "web-view-pane" // see app.css
-    spacing = 3
-    padding = Insets(3)
-    children = List(jokeWebView)
-
   val jokeButton = new Button:
-    prefWidth = 60
+    prefWidth = 80
     prefHeight = 30
-    text = "Joke"
+    text = "New Joke"
     onAction = _ => {
       val task = ChuckNorrisTask(executor, uri)
       jokeProperty <== task.value
-      jokeIndicator.visible <== task.running
+      busyIndicator.visible <== task.running
       this.disable <== task.running
       executor.execute(task)
     }
 
-  val jokeIndicator = new ProgressIndicator:
+  val busyIndicator = new ProgressIndicator:
     prefWidth = 60
     prefHeight = 30
     progress = -1.0
     visible = false
 
-  val jokeToolBar = new ToolBar:
+  val toolbar = new ToolBar:
     prefHeight = 40
-    content = List(jokeButton, new Separator(), jokeIndicator)
+    content = List(jokeButton, new Separator(), busyIndicator)
+
+  val webviewPane = new VBox:
+    id = "web-view-pane" // see app.css
+    spacing = 3
+    padding = Insets(3)
+    children = List(webview)
 
   val contentPane = new VBox:
     spacing = 6
     padding = Insets(6)
-    children = List(jokeToolBar, jokeWebViewPane)
+    children = List(toolbar, webviewPane)
 
   val scene = new Scene:
     root = contentPane
